293fe0a0bc596942e7dd76c2d188b913d3e13c22
MD MONIRUL ISLAM <monirul.dev@gmail.com>
2025-09-14 01:54:57 +0600

    Up to Add Home Search Box

---
+++ components/SearchBar.tsx
@@ -0,0 +1,30 @@
+import { View, Image, TextInput } from "react-native";
+import React from "react";
+import { icons } from "@/constants/icons";
+
+interface Props {
+  onPress?: () => void;
+  placeholder: string;
+}
+
+const SearchBar = ({onPress, placeholder}: Props) => {
+  return (
+    <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
+      <Image
+        source={icons.search}
+        className="w-5 h-5"
+        resizeMode="contain"
+        tintColor="#AB8BFF"
+      />
+      <TextInput 
+      onPress={onPress}
+      placeholder={placeholder}
+      value=""
+      placeholderTextColor="#AB8BFF"
+      className="flex-1 ml-2 text-white"
+      />
+    </View>
+  );
+};
+
+export default SearchBar;
